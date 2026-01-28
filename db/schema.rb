1c2ccc058e06145cd806eb961e126ddfb25f6fb1
Luca Forni <l.forni@modalsource.com>
2026-01-29 00:16:04 +0100

    Fix: Correct schema version format in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20260128_120001) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_28_120001) do
   create_table "additional_route_endpoints", id: :integer, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "route_id"
     t.string "endpoint_type"
